6f806ca4b649561308e8bdbb4914eeb0d7b16864
stka23aiml-beep <stka23aiml@cmrit.ac.in>
2026-06-22 23:22:53 +0530

    Program10 Azure Pipeline

---
+++ src/main/java/com/program2/maven/program22_example_jar/App.java
@@ -0,0 +1,10 @@
+package com.program2.maven.program22_example_jar;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
